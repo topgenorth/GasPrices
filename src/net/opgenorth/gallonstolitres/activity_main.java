113d8429c891d76d2207fc9cde2c1766ceca8716
Tom Opgenorth <tom@opgenorth.net>
2012-07-30 05:00:46 -0700

    display the Canadian price in cents/litre

--- src/net/opgenorth/gallonstolitres/activity_main.java
+++ src/net/opgenorth/gallonstolitres/activity_main.java
@@ -35,15 +35,14 @@ public class activity_main extends Activity {
                     double litres = Converter.toLitres(1.0);
 
                     double cadPerLitre = cad/litres;
-                    DecimalFormat f = new DecimalFormat("#0.00");
+                    DecimalFormat f = new DecimalFormat("###0");
 
-                    _canadianPrice.setText(f.format(cadPerLitre));
+                    _canadianPrice.setText(f.format(cadPerLitre*100) + " cents per litre.");
                     }
                     catch (NumberFormatException nfe)
                     {
                         _canadianPrice.setText("0");
                     }
-
                 }
 
                 return false;
